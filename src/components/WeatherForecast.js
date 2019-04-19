2d47962f22c3b47b62a94713ff8839ccaf1926ed
viet nguyen <viet34tqc@gmail.com>
2019-04-20 01:03:56 +0700

    lấy dữ liệu forecast khi chọn tọa độ hiện tại

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -8,29 +8,36 @@ const appid = "8a548118fb12d8549d52d4d6887f9937";
 export class WeatherForecast extends Component {
   constructor(props) {
     super(props);
-    this.lat = "";
-    this.long = "";
+    this.url = "";
+    this.state = {
+      temperature: "",
+      city: "",
+      humidity: "",
+      description: "",
+      day: "",
+      lat: "",
+      long: ""
+    };
   }
-  state = {
-    temperature: "",
-    city: "",
-    humidity: "",
-    description: "",
-    day: ""
-  };
-  getWeather = e => {
+
+  getTodayWeather = e => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const isGetCurrentPosition = e.target.elements.currentPosition.checked;
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}&units=metric`;
+    this.url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}&units=metric`;
     if (isGetCurrentPosition) {
       this.getCurrentPosition();
     } else {
-      this.getWeatherData(url);
+      // Set lại lat long về 0 để tránh trường hợp choose current position rồi bỏ đi thì nó vẫn lưu giá trị.
+      this.setState({
+        lat: "",
+        long: ""
+      });
+      this.getTodayWeatherData(this.url);
     }
   };
 
-  getWeatherData = async url => {
+  getTodayWeatherData = async url => {
     const api_call = await fetch(url);
     const response = await api_call.json();
     if (response) {
@@ -45,36 +52,8 @@ export class WeatherForecast extends Component {
     }
   };
 
-  getCurrentPosition = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this.getLocation);
-    } else {
-      console.log("Geolocation is not supported");
-    }
-  };
-
-  getLocation = position => {
-    const lat = position.coords.latitude;
-    const long = position.coords.longitude;
-    const url = `http://api.openweathermap.org/data/2.5/weather?&appid=${appid}&lat=${lat}&lon=${long}&units=metric`;
-    this.getWeatherData(url);
-  };
-
-  componentDidUpdate() {
-    this.root = ReactDOM.findDOMNode(this);
-    this.city = this.root.querySelector("#city").value;
-  }
-
-  getDay = async day => {
-    const city = this.city;
-    // Nếu là ngày hôm nay thì trả lại dữ liệu đã lấy trước đó.
-    if (day === "0") {
-      this.setState(this.todayData);
-      return;
-    }
-    const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&appid=${appid}&units=metric&cnt=${day}`
-    );
+  getDaysWeatherData = async url => {
+    const api_call = await fetch(url);
     const response = await api_call.json();
     if (response) {
       const wantedDay = response.list.slice(-1)[0];
@@ -89,6 +68,46 @@ export class WeatherForecast extends Component {
     }
   };
 
+  getCurrentPosition = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.getLocation);
+    } else {
+      console.log("Geolocation is not supported");
+    }
+  };
+
+  getLocation = position => {
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    this.setState({
+      lat,
+      long
+    });
+    this.url = `http://api.openweathermap.org/data/2.5/weather?&appid=${appid}&lat=${lat}&lon=${long}&units=metric`;
+    this.getTodayWeatherData(this.url);
+  };
+
+  componentDidUpdate() {
+    this.root = ReactDOM.findDOMNode(this);
+    this.city = this.root.querySelector("#city").value;
+  }
+
+  getWeatherByDays = async day => {
+    const city = this.city;
+    this.url = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&appid=${appid}&units=metric&cnt=${day}`;
+    if (this.state.lat && this.state.long) {
+      this.url = `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${
+        this.state.lat
+      }&lon=${this.state.long}&appid=${appid}&units=metric&cnt=${day}`;
+    }
+    // Nếu là ngày hôm nay thì trả lại dữ liệu đã lấy trước đó.
+    if (day === "0") {
+      this.setState(this.todayData);
+      return;
+    }
+    this.getDaysWeatherData(this.url);
+  };
+
   render() {
     const weatherInfo = {
       temperature: this.state.temperature,
@@ -99,8 +118,11 @@ export class WeatherForecast extends Component {
     };
     return (
       <div>
-        <Location getWeather={this.getWeather} />
-        <ForecastResult weatherInfo={weatherInfo} getDay={this.getDay} />
+        <Location getWeather={this.getTodayWeather} />
+        <ForecastResult
+          weatherInfo={weatherInfo}
+          getDay={this.getWeatherByDays}
+        />
       </div>
     );
   }
