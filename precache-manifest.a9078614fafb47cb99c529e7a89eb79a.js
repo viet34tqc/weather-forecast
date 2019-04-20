441d5fee2a92915d7589b7f5c635c920090e8f37
viet nguyen <viet34tqc@gmail.com>
2019-04-20 16:08:26 +0700

    Updates

---
+++ precache-manifest.a9078614fafb47cb99c529e7a89eb79a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c753dce0acc1d73f011b",
+    "url": "/weather-forecast/static/js/runtime~main.ff5e48c9.js"
+  },
+  {
+    "revision": "4abf22c3fac058c3c61a",
+    "url": "/weather-forecast/static/js/main.6aa38273.chunk.js"
+  },
+  {
+    "revision": "dfd10b7e1ad0b1a8b44a",
+    "url": "/weather-forecast/static/js/2.2ac306c6.chunk.js"
+  },
+  {
+    "revision": "4abf22c3fac058c3c61a",
+    "url": "/weather-forecast/static/css/main.29b6a429.chunk.css"
+  },
+  {
+    "revision": "12a4f5d401a22e18982514502147883c",
+    "url": "/weather-forecast/index.html"
+  }
+];
